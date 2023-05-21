7a55eb04a4406ce14395436f49f15e1447fe181d
cosmicmumbear <105939075+cosmicmumbear@users.noreply.github.com>
2023-05-21 20:39:07 +0200

    main menu and about views added

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+   
+   public void LoadGame()
+   {
+       SceneManager.LoadScene(2);
+   }
+
+    public void LoadMainMenu()
+   {
+       SceneManager.LoadScene(0);
+   }
+
+     public void LoadAbout()
+   {
+       SceneManager.LoadScene(1);
+   }
+
+    
+}
