cea2b0cad4db0491366216f06b00e4876c75adcf
cosmicmumbear <105939075+cosmicmumbear@users.noreply.github.com>
2023-05-21 21:30:51 +0200

    gameover handler added

--- Scripts/GameOverHandler.cs
+++ Scripts/GameOverHandler.cs
@@ -8,12 +8,15 @@ public class GameOverHandler : MonoBehaviour
 {
 
     [SerializeField] private GameObject gameOverDisplay;
-       [SerializeField] private GameObject player;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject bonusSpawner;
 
  
     public void EndGame()
     {
         gameOverDisplay.gameObject.SetActive(true);
+        bonusSpawner.gameObject.SetActive(false);
+        player.gameObject.SetActive(false);
            
     }
 
